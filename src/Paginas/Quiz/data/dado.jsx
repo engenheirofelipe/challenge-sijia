87dc933b4c4243e38d939a81f1e3b80c27d00f2d
Felipe <felipeferreira.tech@outlook.com>
2025-08-19 18:36:31 -0300

    Atualizando data

--- src/Paginas/Quiz/data/dado.jsx
+++ src/Paginas/Quiz/data/dado.jsx
@@ -32,6 +32,66 @@ const data = [
         },
       ],
     },
+
+    
+    {
+      category: "Ecocardiograma",
+      questions: [
+        {
+          question: "Qual é o exame que utiliza ondas especias ?",
+          options: ["Hemodiálise", "Ecocardiograma", "Tomografia", "Ressonância Magnética"],
+          answer: "Ecocardigrama",
+          tip: "Faz imagens do coração",
+        },
+        {
+          question: "Quem realiza o exame ?",
+          options: ["Balconista", "Super-médico de corações", "Enfermeira", "Super-médico de pulmão"],
+          answer: "Super-médico de corações",
+        },
+        {
+          question: "Pra que o exame serve ?",
+          options: ["Para medir a febre", "Para ver a barriguinha", "Para ver os olhos", "Para descobrir se o nosso coração está funcionando direitinho"],
+          answer: "Para descobrir se o nosso coração está funcionando direitinho",
+        },
+      ],
+    },
+
+
+    {
+      category: "Tomografia",
+      questions: [
+        {
+          question: "Imagine que você é um detetive e precisa de uma máquina especial para tirar fotos dentro do corpo humano. Que máquina é essa?",
+          options: [
+            "Máquina de fazer pipoca",
+            "Máquina de fazer algodão doces",
+            "Máquina de fritar batata",
+            "Tomografia Computadorizada",
+          ],
+          answer: "Tomografia Computadorizada",
+        },
+        {
+          question: "Que som você ouve quando está dentro da máquina de Tomografia?",
+          options: ["Um zumbido suave enquanto os raios-X tiram fotos", "Palmas", "O som de um rio correndo", "O som de chuva"],
+          answer: "Um zumbido suave enquanto os raios-X tiram fotos",
+        },
+        {
+          question: "O que a Tomografia Computadorizada pode detectar que outros exames não conseguem?",
+          options: ["Receitas secretas de bolo", "Problemas dentro do nosso corpinho", "Livros de magia", "Carros de corrida"],
+          answer: "float",
+        },
+        {
+          question: "Quando você está dentro da máquina de Tomografia, como deve se comportar?",
+          options: ["Cantando alto para animar o ambiente", "Ficando bem quietinho, como um espião", "Fazendo piadas engraçadas", "Correr"],
+          answer: "Ficando bem quietinho, como um espião",
+        },
+        {
+          question:"O que os médicos podem fazer com as imagens obtidas através da Tomografia ?",
+          options: ["Criar histórias em quadrinhos", "Verificar se voçê têm superpoderes", "Analisar seu corpinho para te curar", "Ver os cabelos"],
+          answer: "Analisar seu corpinho para te curar",
+        },
+      ],
+    },
   ];
   
   export default data;
--- src/Paginas/Quiz/data/quiz-data.js
+++ src/Paginas/Quiz/data/quiz-data.js
@@ -34,30 +34,10 @@ export let questoesGeral =  [
     ],
     correct: 2,
   },
+  
+  
   {
     id: 4,
-    question: "Dentro da máquina mágica de tomografia, como devemos nos comportar?",
-    answers: [
-      "Ficando mais quietinho possível", 
-      "Pulando e gritando", 
-      "Gritando bem alto", 
-      "Escondido",
-    ],
-    correct: 0,
-  },
-  {
-    id: 5,
-    question: "Quando você está dentro da máquina mágica do raio-x, que som você ouve?",
-    answers: [
-      "O som de um pássaro cantando", 
-      "Um barulhinho engraçado, como um martelinho tocando", 
-      "Fica frio Ai!!!", 
-      "Som de chuva",
-    ],
-    correct: 1,
-  },
-  {
-    id: 6,
     question:"O que os médicos podem fazer com as imagens obtidas através da Tomografia ?",
     answers: [
       "Criar histórias em quadrinhos",
@@ -67,19 +47,9 @@ export let questoesGeral =  [
     ],
     correct: 2,
   },
+  
   {
-    id: 7,
-    question: "Qual é o exame que utiliza ondas especias para ver o nosso corpinho?",
-    answers: [
-      "Hemodiálise", 
-      "Ecocardiograma", 
-      "Escavadeira", 
-      "Batedeira de bolo",
-    ],
-    correct: 1,
-  },
-  {
-    id: 8,
+    id: 5,
     question: "Quem realiza o exame de eletrocardiograma?",
     answers: [
       "Balconista", 
@@ -90,7 +60,7 @@ export let questoesGeral =  [
     correct: 1,
   },
   {
-    id: 9,
+    id: 6,
     question: "Pra que eletrocardiograma exame serve ?",
     answers: [
       "Para medir a febre", 
@@ -101,51 +71,7 @@ export let questoesGeral =  [
     correct: 3,
   },
   {
-    id: 10,
-    question: "Imagine que você é um detetive e precisa de uma máquina especial para tirar fotos dentro do corpo humano. Que máquina é essa?",
-    answers: [
-      "Máquina de fazer pipoca",
-      "Máquina de fazer algodão doces",
-      "Livre Estou, Livre Estou (Elsa, Frozen)",
-      "Tomografia ",
-    ],
-    correct: 3,
-  },
-  {
-    id: 11,
-    question: "Que som você ouve quando está dentro da máquina de Tomografia?",
-    answers: [
-      "Um zumbido suave enquanto os raios-X tiram fotos",
-      "Onde você está, BB?",
-      "O som de um rio correndo",
-      "O som de chuva",
-    ],
-    correct: 0,
-  },
-  {
-    id: 12,
-    question: "O que a Tomografia Computadorizada pode detectar que outros exames não conseguem?",
-    answers: [
-      "Receitas secretas de bolo",
-      "Problemas dentro do nosso corpinho",
-      "Livros de magia",
-      "Carros de corrida",
-    ],
-    correct: 1,
-  },
-  {
-    id: 13, 
-    question: "O que é um exame de raio-x?",
-    answers: [
-      "Tirar uma foto especial do interior do corpo",
-      "Ler um livro de histórias",
-      "Escutar música ",
-      "Comer uma comida d,eliciosa"
-    ],
-    correct: 0,
-  },
-  {
-    id: 14, 
+    id: 7, 
     question: "Quem vai te receber e explicar tudo quando você chega ao lugar do exame?",
     answers: [
       "Um professor",
@@ -155,106 +81,6 @@ export let questoesGeral =  [
     ],
     correct: 1,
   },
-  {
-    id: 15, 
-    question: "Como é a máquina de raio-x?",
-    answers: [
-      "Uma bicicleta",
-      "Uma câmera",
-      "Um computador",
-      "Um robô",
-    ],
-    correct: 1,
-  },
-  {
-    id: 16, 
-    question: "O que o técnico vai pedir para você fazer enquanto a foto é tirada?",
-    answers: [
-      "Cantar uma música",
-      "Pular bem alto",
-      "Ficar bem quietinho",
-      "Dançar",
-    ],
-    correct: 2,
-  },
-  {   
-    id: 17,
-    question: "O que o médico faz com as fotos tiradas pelo raio-x?",
-    answers: [
-      "Pinta as fotos",
-      "Olha para elas em uma tela de computador",
-      "Coloca em um álbum de fotos",
-      "Esconde as fotos",
-    ],
-    correct: 1,
-  },
-  {
-    id: 18,
-    question: "Quando você está dentro da máquina de Tomografia, como deve se comportar?",
-    answers: [
-      "Cantando alto para animar o ambiente",
-      "Ficando bem quietinho, como um espião",
-      "Fazendo piadas engraçadas",
-      "Correr pela sala",
-    ],
-    correct: 1,
-  },
-  {
-    id: 19,  
-    question: "O que é um hemograma?",
-    answers: [
-      "Um exame que vigia o seu corpo",
-      "Uma festa de aniversário",
-      "Um jogo de futebol",
-      "Uma viagem de férias",
-    ],
-    correct: 0,
-  },
-  {
-    id: 20,  
-    question: "Quem pode te acompanhar quando você vai fazer um hemograma?",
-    answers: [
-      "Um monstro assustador",
-      "Um amigo de aventuras",
-      "Um desconhecido",
-      "Um animal de estimação",
-    ],
-    correct: 1,
-  },
-  {
-    id: 21,  
-    question: "Como é a sensação quando tiram uma gotinha do seu sangue?",
-  
-    answers: [
-      "Dói muito",
-      "É como se alguém te desse um carinho bem rápido",
-      "Faz cócegas  ",
-      "É como um abraço apertado",
-    ],
-    correct: 1,
-  },
-  {
-    id: 22,  
-    question: "Para onde vai o sangue depois que tiram uma gotinha?",
-    answers: [
-      "Para o parque",
-      "Para o laboratório",
-      "Para a escola",
-      "Para a loja de brinquedos",
-    ],
-    correct: 1,
-  },
-  {
-    id: 23,  
-    question: "Quais são os três tipos de 'amiguinhos' que os médicos procuram no sangue?",
-    answers: [
-      "Glóbulos vermelhos, glóbulos brancos e plaquetas  ",
-      "Carros, aviões e trens  ",
-      "Doces, chocolates e sorvetes  ",
-      "Peixes, pássaros e cachorros",
-    ],
-    correct: 0,
-  } 
 ];
 
 
